faa40fc42b0abaa71c8a545a195202fba46c81d6
Sangmin Song <awd1779@gmail.com>
2022-09-10 11:57:18 +1000

    1

---
+++ 221.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import cv2
+
+cap = cv2.VideoCapture(0)
+resolutions = {}
+for index, row in table[["W", "H"]].iterrows():
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, row["W"])
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, row["H"])
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    resolutions[str(width)+"x"+str(height)] = "OK"
+print(resolutions)
